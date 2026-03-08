f0829d78d4e354298fa5bb09bba14184985b7a2f
User Evgeniu <k0veugene@yandex.com>
2026-03-08 19:22:51 +0300

    fix

--- sprint1/problems/map_json/solution/src/http_server.h
+++ sprint1/problems/map_json/solution/src/http_server.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "sdk.h"
 
 #define BOOST_BEAST_USE_STD_STRING_VIEW
@@ -85,14 +84,61 @@ template <typename RequestHandler>
 class Listener : public std::enable_shared_from_this<Listener<RequestHandler>> {
 public:
     template <typename Handler>
-    Listener(net::io_context& ioc, const tcp::endpoint& endpoint, Handler&& request_handler);
+    Listener(net::io_context& ioc, const tcp::endpoint& endpoint, Handler&& request_handler)
+        : ioc_(ioc)
+        , acceptor_(net::make_strand(ioc))
+        , request_handler_(std::forward<Handler>(request_handler)) {
 
-    void Run();
+        beast::error_code ec;
+
+        acceptor_.open(endpoint.protocol(), ec);
+        if (ec)
+            throw std::runtime_error("Failed to open acceptor");
+
+        acceptor_.set_option(net::socket_base::reuse_address(true), ec);
+        if (ec)
+            throw std::runtime_error("Failed to set option");
+
+        acceptor_.bind(endpoint, ec);
+        if (ec)
+            throw std::runtime_error("Failed to bind");
+
+        acceptor_.listen(net::socket_base::max_listen_connections, ec);
+        if (ec)
+            throw std::runtime_error("Failed to listen");
+    }
+
+    void Run() {
+        DoAccept();
+    }
 
 private:
-    void DoAccept();
-    void OnAccept(beast::error_code ec, tcp::socket socket);
-    void AsyncRunSession(tcp::socket&& socket);
+    void DoAccept() {
+        acceptor_.async_accept(
+            net::make_strand(ioc_),
+            beast::bind_front_handler(
+                &Listener::OnAccept,
+                this->shared_from_this()
+            )
+        );
+    }
+
+    void OnAccept(beast::error_code ec, tcp::socket socket) {
+        if (ec) {
+            std::cerr << "Accept error: " << ec.message() << std::endl;
+            return;
+        }
+
+        AsyncRunSession(std::move(socket));
+        DoAccept();
+    }
+
+    void AsyncRunSession(tcp::socket&& socket) {
+        std::make_shared<Session<RequestHandler>>(
+            std::move(socket),
+            request_handler_
+        )->Run();
+    }
 
     net::io_context& ioc_;
     tcp::acceptor acceptor_;
@@ -110,4 +156,4 @@ void ServeHttp(net::io_context& ioc, const tcp::endpoint& endpoint, RequestHandl
     )->Run();
 }
 
-} 
+}
