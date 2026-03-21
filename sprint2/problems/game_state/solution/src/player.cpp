b67fc109cea7d762e4e9dfdc1372028ec63dda09
User Evgeniu <k0veugene@yandex.com>
2026-03-22 01:52:48 +0300

    fix

--- sprint2/problems/game_state/solution/src/player.cpp
+++ sprint2/problems/game_state/solution/src/player.cpp
@@ -22,9 +22,9 @@ Player& PlayerManager::AddPlayer(std::string name, const Map::Id& map_id) {
     Player& p = players_.back();
 
     token_to_player_[p.GetToken()] = &p;
-    p.SetPosition({0.0, 0.0});
-    p.SetSpeed({0.0, 0.0});
-    p.SetDirection(Direction::NORTH);
+    p.GetPosition({0.0, 0.0});
+    p.GetSpeed({0.0, 0.0});
+    p.GetDirection(Direction::NORTH);
 
     return p;
 }
