efe6fa7867d0a636a984b85f5ea343126928462b
User Evgeniu <k0veugene@yandex.com>
2026-04-12 15:42:24 +0300

    fix

--- sprint3/problems/gen_objects/solution/src/json_loader.h
+++ sprint3/problems/gen_objects/solution/src/json_loader.h
@@ -6,6 +6,6 @@
 
 namespace json_loader {
 
-model::Game LoadGame(const std::filesystem::path& json_path);
+model::Game LoadGame(const std::filesystem::path& json_path, extra_data::Storage& extra_storage);
 
 }  // namespace json_loader
