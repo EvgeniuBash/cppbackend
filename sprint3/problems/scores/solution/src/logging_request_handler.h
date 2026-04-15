53e3579a2a091f25c2a4eee7972b2e8f121ef6d1
User Evgeniu <k0veugene@yandex.com>
2026-04-15 04:41:08 +0300

    fix

---
+++ sprint3/problems/scores/solution/src/logging_request_handler.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "logger.h"
+#include <chrono>
+#include <utility>
+
+namespace logging = boost::log;
+namespace json = boost::json;
+
+template <typename Handler>
+class LoggingRequestHandler {
+public:
+    explicit LoggingRequestHandler(Handler& handler)
+        : handler_(handler) {}
+
+    template <typename Request, typename Send>
+    void operator()(Request&& req, Send&& send) {
+
+        auto start = std::chrono::steady_clock::now();
+
+        json::object req_data{
+            {"ip", "unknown"},
+            {"URI", std::string(req.target())},
+            {"method", std::string(req.method_string())}
+        };
+
+        BOOST_LOG_TRIVIAL(info)
+            << boost::log::add_value(additional_data, req_data)
+            << "request received";
+
+        handler_(
+            std::forward<Request>(req),
+            [start, send = std::forward<Send>(send)](auto&& response) mutable {
+
+                auto end = std::chrono::steady_clock::now();
+                auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+
+                json::object resp_data{
+                    {"response_time", duration},
+                    {"code", response.result_int()}
+                };
+
+                if (response.base().find(boost::beast::http::field::content_type) != response.base().end()) {
+                    resp_data["content_type"] =
+                        std::string(response.base()[boost::beast::http::field::content_type]);
+                } else {
+                    resp_data["content_type"] = nullptr;
+                }
+
+                BOOST_LOG_TRIVIAL(info)
+                    << boost::log::add_value(additional_data, resp_data)
+                    << "response sent";
+
+                send(std::forward<decltype(response)>(response));
+            }
+        );
+    }
+
+private:
+    Handler& handler_;
+};
